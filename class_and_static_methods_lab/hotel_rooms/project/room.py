1e8cf59feeb7a293edc788b3fb28cd1fad198181
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-05 00:23:18 +0300

    Update room.py

--- class_and_static_methods_lab/hotel_rooms/project/room.py
+++ class_and_static_methods_lab/hotel_rooms/project/room.py
@@ -10,14 +10,11 @@ class Room:
         if self.is_taken or self.capacity < people:
             return f"Room number {self.number} cannot be taken"
         self.is_taken = True
-        self.guests += people
-        return self.guests
+        self.guests = people
 
     def free_room(self):
         if not self.is_taken:
             return f"Room number {self.number} is not taken"
         self.is_taken = False
-        guests = self.guests
         self.guests = 0
-        return guests
 
