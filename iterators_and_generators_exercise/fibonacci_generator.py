4a732d1bb65fe3e107f5b83403025f1595733804
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-10-21 23:08:05 +0300

    Create fibonacci_generator.py

---
+++ iterators_and_generators_exercise/fibonacci_generator.py
@@ -0,0 +1,15 @@
+def fibonacci():
+
+    x, y = 0, 1
+    while True:
+        yield x
+        x, y = y, x + y
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
+generator = fibonacci()
+for i in range(1):
+    print(next(generator))
