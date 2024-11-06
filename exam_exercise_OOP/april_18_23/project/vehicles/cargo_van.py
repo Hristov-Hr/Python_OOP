d23eec93bedd1ac7b6216544e6a10cb230e07966
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-11-07 00:25:15 +0200

    Update cargo_van.py

---
+++ exam_exercise_OOP/april_18_23/project/vehicles/cargo_van.py
@@ -0,0 +1,10 @@
+from project.vehicles.base_vehicle import BaseVehicle
+
+
+class CargoVan(BaseVehicle):
+
+    def __init__(self, brand, model, license_plate_number):
+        super().__init__(brand, model, license_plate_number, max_mileage=180)
+
+    def drive(self, mileage: float):
+        self.battery_level -= round(mileage / self.max_mileage * 100) + 5
