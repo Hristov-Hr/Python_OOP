33e338020ef703daaf78ec79b0f2d1e5163f97aa
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-11-09 19:09:33 +0200

    Update base_vehicle.py

--- exam_exercise_OOP/april_18_23/project/vehicles/base_vehicle.py
+++ exam_exercise_OOP/april_18_23/project/vehicles/base_vehicle.py
@@ -11,6 +11,36 @@ class BaseVehicle(ABC):
         self.battery_level: int = 100
         self.is_damaged: bool = False
 
+    @property
+    def brand(self):
+        return self.__brand
+
+    @brand.setter
+    def brand(self, value):
+        if not value.strip():
+            raise ValueError("Brand cannot be empty!")
+        self.__brand = value
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, value):
+        if not value.strip():
+            raise ValueError("Model cannot be empty!")
+        self.__model = value
+
+    @property
+    def license_plate_number(self):
+        return self.__license_plate_number
+
+    @license_plate_number.setter
+    def license_plate_number(self, value):
+        if not value.strip():
+            raise ValueError("License plate number is required!")
+        self.__license_plate_number = value
+
     @abstractmethod
     def drive(self, mileage: float):
         ...
@@ -19,7 +49,7 @@ class BaseVehicle(ABC):
         self.battery_level = 100
 
     def change_status(self):
-        self.is_damaged = -self.is_damaged
+        self.is_damaged = False if self.is_damaged else True
 
     def __str__(self):
         status = "Damaged" if self.is_damaged else "OK"
